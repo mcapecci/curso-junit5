c9a1e7d98557605dffb415417fad87573b70ba22
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-03-10 09:16:45 -0300

    Timeout en JUnit 5

--- src/test/java/org/mcapecci/junit5/domain/CuentaTest.java
+++ src/test/java/org/mcapecci/junit5/domain/CuentaTest.java
@@ -10,10 +10,12 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mcapecci.junit5.exceptions.DineroInsuficienteException;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
@@ -382,5 +384,26 @@ class CuentaTest {
     static List<String> montoList() {
         return Arrays.asList("100", "200", "300", "500", "700", "1000.12345");
     }
+    @Nested
+    @Tag("timeout")
+    class EjemploTimeoutTest{
+        @Test
+        @Timeout(1)
+        void pruebaTimeout() throws InterruptedException {
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
 
+        @Test
+        @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+        void pruebaTimeout2() throws InterruptedException {
+            TimeUnit.MILLISECONDS.sleep(900);
+        }
+
+        @Test
+        void testTimeoutAssertions() {
+            assertTimeout(Duration.ofSeconds(5), ()->{
+                TimeUnit.MILLISECONDS.sleep(4000);
+            });
+        }
+    }
 }
